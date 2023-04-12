68597279ba105e6e6b2489c1478beef3ffe287dd
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-04-12 00:46:30 -0500

    Add postData funcion and fix render onload

--- src/index.js
+++ src/index.js
@@ -6,6 +6,56 @@ const getDataServer = async () => {
       method: 'GET'
     })
   } catch (error) {
-    
+    console.log('Error')   
   }
 }
+
+const scores = { //this has to be the data received by API
+    "result": [
+        {
+            "user": "John Doe",
+            "score": 42
+        },
+        {
+            "user": "Peter Parker",
+            "score": 35
+        },
+        {
+            "user": "Wonder Woman",
+            "score": 50
+        }
+    ]
+};
+
+const generateScoreItem = (score) => {
+  let itemScore = `<div>${score.user}:${score.score}</div>`;
+  return itemScore;
+};
+
+const renderScores = () => {
+  const scoreContainer = document.getElementById('scoresContainer');
+  scoreContainer.innerHTML = '';
+  scores.result.forEach((score) => {
+    let itemScore = generateScoreItem(score);
+    scoreContainer.innerHTML += itemScore;
+  });
+};
+
+const postDataServer = async (inputData) => {
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hBfPl1kjDZ5eEwBSxYij/scores/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputData),
+    });
+    const responseReceived = await response.json();
+    console.log(responseReceived);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+postDataServer({ "user": "Wonder Woman", "score": 50 })
+renderScores();
