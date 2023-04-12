347336debeeb48ec3a396a081859d2971ed79965
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-04-11 20:04:45 -0500

    add getData function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+const getDataServer = async () => {
+  try {
+    const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hBfPl1kjDZ5eEwBSxYij/scores/', {
+      method: 'GET'
+    })
+  } catch (error) {
+    
+  }
+}
